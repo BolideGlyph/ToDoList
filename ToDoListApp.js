f4f9c44e6a535ea9de907d9179d3c0a058dc33f0
Jeremy Barbe <littlejssn@gmail.com>
2015-10-21 20:07:15 +0700

    changed some stuff, mostly just scared of proceeding without a push

--- ToDoListApp.js
+++ ToDoListApp.js
@@ -1,85 +1,136 @@
 var radioInput = "";
 var textInput = "";
 var frequencyInput = "";
+var perIntervalInput = "";
 
 
-//function getTableData() {
-//$('#activityTable > tbody:first').append("<tr> <td>" + textInput + "</td> <td>" + radioInput + "</td> </tr> <br>");
-//};
+//FUNCTION USED TO TAKE INPUT FROM FORM AND PUT IT INTO A TABLE ROW
 
 function getTableData() {
-if(frequencyInput === "Daily") {
-$("#daily").append("<div class=\"Row\">" +
-			        "<div class=\"Cell\">" +
-			            "<p>" + textInput + "</p>" +
-			        "</div>" +
-			        "<div class=\"Cell\">" +
-			            "<p>" + radioInput + "</p>" +
-			        "</div>" +
-			        "<div class=\"Cell\">" +
-			            "<button class=\"deleteButton\">Delete</button>" +
-			        "</div>" +
-			        "</div>");
+	if(frequencyInput === "Daily") {
+	$("#daily").append("<div class=\"Row\">" +
+				        "<div class=\"Cell\">" +
+				            "<p>" + textInput + "</p>" +
+				        "</div>" +
+				        "<div class=\"Cell\">" +
+				        	"<p>" + perIntervalInput + "</p>" +
+				        "</div>" +
+				        "<div class=\"Cell\">" +
+				            "<p>" + radioInput + "</p>" +
+				        "</div>" +
+				        "<div class=\"Cell\">" +
+				            "<button class=\"deleteButton\">Delete</button>" +
+				        "</div>" +
+				        "</div>");
 
-$('.deleteButton').click(function(){
-   $(this).parent().parent().remove();
-});
-}
-else if(frequencyInput === "Weekly") {
-$("#weekly").append("<div class=\"Row\">" +
-			        "<div class=\"Cell\">" +
-			            "<p>" + textInput + "</p>" +
-			        "</div>" +
-			        "<div class=\"Cell\">" +
-			            "<p>" + radioInput + "</p>" +
-			        "</div>" +
-			        "<div class=\"Cell\">" +
-			            "<button class=\"deleteButton\">Delete</button>" +
-			        "</div>" +
-			        "</div>");
+	$('#dailyHeader').show();
 
-$('.deleteButton').click(function(){
-   $(this).parent().parent().remove();
-});
+	$('.deleteButton').click(function(){
+	   $(this).parent().parent().remove();
+	});
+	}
 
-}
-else if(frequencyInput === "Monthly") {
-$("#monthly").append("<div class=\"Row\">" +
-			        "<div class=\"Cell\">" +
-			            "<p>" + textInput + "</p>" +
-			        "</div>" +
-			        "<div class=\"Cell\">" +
-			            "<p>" + radioInput + "</p>" +
-			        "</div>" +
-			        "<div class=\"Cell\">" +
-			            "<button class=\"deleteButton\">Delete</button>" +
-			        "</div>" +
-			        "</div>");
+	else if(frequencyInput === "Weekly") {
+	$("#weekly").append("<div class=\"Row\">" +
+				        "<div class=\"Cell\">" +
+				            "<p>" + textInput + "</p>" +
+				        "</div>" +
+				        "<div class=\"Cell\">" +
+				        	"<p>" + perIntervalInput + "</p>" +
+				        "</div>" +
+				        "<div class=\"Cell\">" +
+				            "<p>" + radioInput + "</p>" +
+				        "</div>" +
+				        "<div class=\"Cell\">" +
+				            "<button class=\"deleteButton\">Delete</button>" +
+				        "</div>" +
+				        "</div>");
 
-$('.deleteButton').click(function(){
-   $(this).parent().parent().remove();
-});
+	$('#weeklyHeader').show();
 
-}
-else if(frequencyInput === "Yearly") {
-$("#yearly").append("<div class=\"Row\">" +
-			        "<div class=\"Cell\">" +
-			            "<p>" + textInput + "</p>" +
-			        "</div>" +
-			        "<div class=\"Cell\">" +
-			            "<p>" + radioInput + "</p>" +
-			        "</div>" +
-			        "<div class=\"Cell\">" +
-			            "<button class=\"deleteButton\">Delete</button>" +
-			        "</div>" +
-			        "</div>");
+	$('.deleteButton').click(function(){
+	   $(this).parent().parent().remove();
+	});
+	}
 
-$('.deleteButton').click(function(){
-   $(this).parent().parent().remove();
-});
+	else if(frequencyInput === "Monthly") {
+	$("#monthly").append("<div class=\"Row\">" +
+				        "<div class=\"Cell\">" +
+				            "<p>" + textInput + "</p>" +
+				        "</div>" +
+				        "<div class=\"Cell\">" +
+				        	"<p>" + perIntervalInput + "</p>" +
+				        "</div>" +
+				        "<div class=\"Cell\">" +
+				            "<p>" + radioInput + "</p>" +
+				        "</div>" +
+				        "<div class=\"Cell\">" +
+				            "<button class=\"deleteButton\">Delete</button>" +
+				        "</div>" +
+				        "</div>");
 
-}
+	$('#monthlyHeader').show();
+
+	$('.deleteButton').click(function(){
+	   $(this).parent().parent().remove();
+	});
+	}
+
+	else if(frequencyInput === "Yearly") {
+	$("#yearly").append("<div class=\"Row\">" +
+				        "<div class=\"Cell\">" +
+				            "<p>" + textInput + "</p>" +
+				        "</div>" +
+				        "<div class=\"Cell\">" +
+				        	"<p>" + perIntervalInput + "</p>" +
+				        "</div>" +
+				        "<div class=\"Cell\">" +
+				            "<p>" + radioInput + "</p>" +
+				        "</div>" +
+				        "<div class=\"Cell\">" +
+				            "<button class=\"deleteButton\">Delete</button>" +
+				        "</div>" +
+				        "</div>");
+
+	$('#yearlyHeader').show();
+
+	$('.deleteButton').click(function(){
+	   $(this).parent().parent().remove();
+	});
+	}
+
+	else if(frequencyInput === "customInterval") {
+	$("#custom").append("<div class=\"Row\">" +
+				        "<div class=\"Cell\">" +
+				            "<p>" + textInput + "</p>" +
+				        "</div>" +
+				        "<div class=\"Cell\">" +
+				        	"<p>" + perIntervalInput + "</p>" +
+				        "</div>" +
+				        "<div class=\"Cell\">" +
+				            "<p>" + radioInput + "</p>" +
+				        "</div>" +
+				        "<div class=\"Cell\">" +
+				            "<button class=\"deleteButton\">Delete</button>" +
+				        "</div>" +
+				        "</div>");
+
+	$("#customHeader").append(numCustomInt.value + " " + customIntSelect.value);
+	$('#customHeader').show();
+
+	/*$("#headerWrapper").append("<p id=\"customHeader\" style=\"display:none\" class=\"tableHeader\"></p>" +
+"<div id=\"custom\" class=\"Table\"></div>");*/
+
+/*dont forget to add headerWrapper Div to html*/
+
+
+	$('.deleteButton').click(function(){
+	   $(this).parent().parent().remove();
+
+	});
+	}
 };
+
 
 
 $('input:radio[class=activityType]').click(function() {
@@ -95,6 +146,13 @@ frequencyInput = $('input:radio[class=activityFrequency]:checked').val();
 $("#submitButton").click(function(e) {
     e.preventDefault();
     textInput = document.getElementById('activity').value;
+
+    if(frequencyInput === "customInterval") {
+    perIntervalInput= customperInterval.value;
+    } else {
+    perIntervalInput= perInterval.value;
+	};
+
  	getTableData();
  	var resetForm = document.getElementById("activityInput").reset();
  	$('#overlayDiv').hide();
@@ -103,8 +161,12 @@ $("#submitButton").click(function(e) {
 
 $("#cancelButton").click(function(e) {
 	e.preventDefault();
+	$("#customIntBox").hide();
 	$('#overlayDiv').hide();
+	var resetForm = document.getElementById("activityInput").reset();
+	
 });
+
 
 
 $('#addActivityButton').click(function(){
@@ -112,6 +174,7 @@ $('#addActivityButton').click(function(){
 });
 
 
-$('.deleteButton').click(function(){
-   $(this).parent().parent().remove();
-});
+
+$("#customInterval").click(function(){
+	$("#customIntBox").show();
+})
